3d17aa731d08345245077551f7e2d2a4908f51ad
SarahPrado <sarahpradomedeiros@gmail.com>
2024-01-11 17:08:42 -0300

    Merge de Header básico com main

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Nav, Container } from 'react-bootstrap';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
+      <footer className="py-5 my-5 border-top">
+        <Row className="row-cols-1 row-cols-sm-2 row-cols-md-5">
+          <Col mb={3}>
+            <a href="/" className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+              <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
+            </a>
+            <p className="text-body-secondary">© 2023</p>
+          </Col>
+          <Col mb={3}></Col>
+          <Col mb={3}>
+            <h5>Section</h5>
+            <Nav className="flex-column">
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Home</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Features</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Pricing</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">FAQs</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">About</Nav.Link></Nav.Item>
+            </Nav>
+          </Col>
+          <Col mb={3}>
+            <h5>Section</h5>
+            <Nav className="flex-column">
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Home</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Features</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Pricing</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">FAQs</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">About</Nav.Link></Nav.Item>
+            </Nav>
+          </Col>
+          <Col mb={3}>
+            <h5>Section</h5>
+            <Nav className="flex-column">
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Home</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Features</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">Pricing</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">FAQs</Nav.Link></Nav.Item>
+              <Nav.Item className="mb-2"><Nav.Link href="#" className="p-0 text-body-secondary">About</Nav.Link></Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+      </footer>
+  );
 }
 
-export default Footer
+export default Footer;
