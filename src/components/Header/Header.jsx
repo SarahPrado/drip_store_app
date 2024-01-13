a6cda993a0c149304d0a0642edb72ee7df270a12
SarahPrado <sarahpradomedeiros@gmail.com>
2024-01-13 17:53:22 -0300

    Sobescrevendo o bootstrap- ajustes visuais no Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Menu from '../Menu/Menu';
+import './Header.css'
 
 const Header = () => {
   return (
@@ -12,19 +13,26 @@ const Header = () => {
     <Navbar expand="lg" className="bg-body-tertiary">
         <Container fluid>
           {/* Digital College à esquerda */}
-          <Navbar.Brand href="#" className="me-auto">Digital College</Navbar.Brand>
+          <Navbar.Brand href="#" className="me-auto">
+          <img src="src/assets/logo/logocompletadigitalcollege.svg" height='55%' alt="" />
+          </Navbar.Brand>
 
           {/* Search no meio */}
           <Form className="d-flex mx-auto">
-            <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
-            <Button variant="outline-success">Search</Button>
+            <Form.Control type="search" placeholder="Search" className="me-2 tamSearch" aria-label="Search"/>
+            <Nav.Link href="#action1">
+              <img src="src/assets/icon/Search.svg" height='55%' alt="" />
+            </Nav.Link>
+
           </Form>
 
           {/* Links à direita */}
           <Nav className="ms-auto" navbarScroll>
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Categorias</Nav.Link>
-            <Nav.Link href="#">Link</Nav.Link>
+            <Nav.Link href="#action1">Cadastre-se</Nav.Link>
+            <Button variant="outline-success">Entrar</Button>
+            <Nav.Link href="#action2">
+              <img className="imgcarrinho" src="src/assets/icon/carrinho.svg" height='55%' alt="" />
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -36,3 +44,4 @@ const Header = () => {
 }
 
 export default Header
+
